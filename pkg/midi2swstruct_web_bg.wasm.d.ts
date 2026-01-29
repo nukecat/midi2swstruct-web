b84f434d98d6dc7e4c32c288d26e10fa64926b10
nukcat <unclearcat@gmail.com>
2026-01-29 22:32:19 +0100

    remove pkg files

--- pkg/midi2swstruct_web_bg.wasm.d.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export const convert_midi_to_structure: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number) => [number, number, number, number];
-export const __wbindgen_externrefs: WebAssembly.Table;
-export const __wbindgen_malloc: (a: number, b: number) => number;
-export const __externref_table_dealloc: (a: number) => void;
-export const __wbindgen_free: (a: number, b: number, c: number) => void;
-export const __wbindgen_start: () => void;
